c7e13e5933656b5a6ad863ffa40cb34b0dc59a0f
Michael <michael.spengler@sap.com>
2022-10-06 05:56:28 +0200

    ensure json comes back

--- node-nlp-server/src/node-nlp-server.ts
+++ node-nlp-server/src/node-nlp-server.ts
@@ -4,14 +4,15 @@ const express = require("express");
 const app = express();
 const nodeNLPService: NodeNLPService = NodeNLPService.getInstance()
 
+// http://116.203.185.185:8081/getresponse/input/CULT
 app.get("/getresponse/input/:input", async (request: any, response: any) => {
     const result: any = await nodeNLPService.getResponse(request.params.input)
     if (result.answer === "") {
         const pleaseHelpMeLearnMessage =
             "I'm not sure enough to give you a specific answer to your request. You might want to improve my training data."
-        response.send(pleaseHelpMeLearnMessage);
+        response.send({answer: pleaseHelpMeLearnMessage});
     } else {
-        response.send(result.answer);
+        response.send({answer: result.answer});
     }
 });
 
