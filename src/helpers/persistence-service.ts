7d4af0c1ab89c6e575fbde8673bd31576abc5516
Michael <michael.spengler@sap.com>
2022-10-13 10:38:16 +0200

    fix certificates access

--- src/helpers/persistence-service.ts
+++ src/helpers/persistence-service.ts
@@ -12,6 +12,9 @@ export class PersistenceService {
         return PersistenceService.instance
     }
 
+    public readonly pathToCerts = `/etc/letsencrypt/live/cultbeast.org`;
+    public readonly pathToCertFile = `${this.pathToCerts}/fullchain.pem`;
+    public readonly pathToKeyFile = `${this.pathToCerts}/privkey.pem`;
     private pathToOperationalData = `${Deno.cwd()}/operational-data`;
     private pathToSubscribers = `${this.pathToOperationalData}/subscribers.json`;
     private pathToReceivedMessages = `${this.pathToOperationalData}/received-messages.json`;
