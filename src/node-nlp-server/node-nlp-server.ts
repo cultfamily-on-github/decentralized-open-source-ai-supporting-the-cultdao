3899bde183c4be372937de018fd6ba32a3bbc2fe
Michael <michael.spengler@sap.com>
2022-10-16 08:12:17 +0200

    reduce amount of logentries

--- src/node-nlp-server/node-nlp-server.ts
+++ src/node-nlp-server/node-nlp-server.ts
@@ -13,7 +13,6 @@ export class CULTNodeNLPServer {
         return CULTNodeNLPServer.instance
     }
 
-
     private app: any
     private port: Number
     private nodeNLPService: NodeNLPService
@@ -34,11 +33,11 @@ export class CULTNodeNLPServer {
 
         // http://65.108.89.236:8081/getresponse/input/CULT // port 8081 currently closed via firewall
         // if someone wants to reuse the nlp part like this, I can open it and potentially introduce api keys to protect this server
-        // people can already reuse those features indirectly - just like the https://cultbeast.org client reuses them. 
+        // people can already reuse those features indirectly - just like the https://cultbeast.org (also a CULT AI client) reuses them. 
         this.app.get("/getresponse/input/:input", async (request: any, response: any) => {
             const result: any = await this.nodeNLPService.getResponse(request.params.input)
             if (result.answer === "" || result.answer === undefined) {
-                console.log(result) // check this when accurracy seems strange
+                // console.log(result) // check this when accurracy seems strange
                 const pleaseHelpMeLearnMessage =
                     "I'm not sure enough to give you a specific answer to your request. You might want to improve my training data: https://github.com/cultfamily-on-github/decentralized-open-source-ai-supporting-the-cultdao/issues/new?assignees=octocat&labels=trainingdata%2Cfaq&template=q-and-a-pair.yaml&title=A+new+example+q+%26+a+pair+is+coming+to+train+the+CULT+Beast."
                 response.send({ answer: pleaseHelpMeLearnMessage });
